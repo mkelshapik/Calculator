d1d120a1047ad2e6e7ea8579a0d226e172603606
Mykhailo Shapovalov <mishashapik24@gmail.com>
2021-11-28 21:26:28 +0200

    initial commit

---
+++ app/src/main/java/com/shapovalov/calculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.shapovalov.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shapovalov.calculator.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
